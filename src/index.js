a81fa438d490233ae7da7c8fb9c739452fe494aa
userzahar <110730909+userzahar@users.noreply.github.com>
2023-03-02 20:30:25 +0200

    dxsax

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+// import './index.css';
+// import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+const root = document.getElementById('root');
+const div = React.createElement('div', { name: 'main', id: 'test', className: "test-class" }, 'muchacha');
 
+ReactDOM.createRoot(root).render(div );
